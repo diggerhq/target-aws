ba84a9a162989eeb0f769652191a04df726254da
motatoes <moe.habib9@gmail.com>
2021-08-10 14:27:04 +0300

    fix ecr repo name

--- main/service-eks.template.tf
+++ main/service-eks.template.tf
@@ -1,6 +1,6 @@
 
 resource "aws_ecr_repository" "{{service_name}}_ecr" {
-  name                 = "${var.ecs_cluster.name}-${var.service_name}"
+  name                 = "${var.app}-${var.environment}-${var.service_name}"
   image_tag_mutability = true
 }
 
