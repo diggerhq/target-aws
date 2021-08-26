f705fc88cf2a6b4fcfd5fb79bf4a7780cf644c59
motatoes <moe.habib9@gmail.com>
2021-08-26 23:09:10 +0300

    fix var name

--- main/service-lambda.template.tf
+++ main/service-lambda.template.tf
@@ -5,7 +5,7 @@ module "service-{{service_name}}" {
   service_name = "{{service_name}}"
   ecr_repo = aws_ecr_repository.lambda_repo
   api_gateway = aws_api_gateway_rest_api.support_lambda_gateway
-  image_uri = var.image_uri
+  image_uri = var.lambda_image_uri
   {% if environment_config.lambda_role %}
   lambda_role = "{{environment_config.lambda_role}}"
   {% else %}
