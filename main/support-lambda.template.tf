f5b1010b5f3e900508acbe49e4e08c3f472cbfd5
motatoes <moe.habib9@gmail.com>
2021-08-27 20:49:01 +0300

    change resource to mutable

--- main/support-lambda.template.tf
+++ main/support-lambda.template.tf
@@ -2,19 +2,9 @@
 
 {% if environment_config.lambda_service_exists %}
 
-  module "support_lambda" {
-    source = "../support/lambda"
-    project_name = var.app
-    environment = var.environment
-    region = var.region
-    image_tag_mutability = "IMMUTABLE"
-    # image_uri = var.lambda_image_uri
-  }
-
-
   resource "aws_ecr_repository" "support_lambda_repo" {
     name                 = "${var.app}-${var.environment}"
-    image_tag_mutability = "IMMUTABLE"
+    image_tag_mutability = "MUTABLE"
   }
 
   # TODO: if routing is enabled for project use the routing gateway
