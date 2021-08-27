5ea15d73454ebdc26dec3425e25519faff05da10
motatoes <moe.habib9@gmail.com>
2021-08-27 19:01:33 +0300

    move support role to new position

--- main/support-lambda.template.tf
+++ main/support-lambda.template.tf
@@ -24,5 +24,25 @@
   }
 
 
+  resource "aws_iam_role" "default_iam_for_lambda" {
+    name_prefix = "${var.app}-${var.environment}-lambda-role"
+
+    assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+  }
+
 
 {% endif %}
